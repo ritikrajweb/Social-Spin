a0d1582f8197b1409af56e109df7530f5bc76577
ritik-raj-gupta-1 <ritikrajamba12@gmail.com>
2026-03-30 09:25:47 +0530

    Launch: Master Build with PWA, Cognitive Engine, and God-Mode

---
+++ sw.js
@@ -0,0 +1,6 @@
+// sw.js - The App Engine
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', (e) => {
+    e.respondWith(fetch(e.request).catch(() => new Response('Offline')));
+});
